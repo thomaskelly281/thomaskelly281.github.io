ac3c48d1080685005037948ce6665e55d865623a
Thomas Kelly <86194082+thomaskelly281@users.noreply.github.com>
2022-03-04 17:04:45 +0000

    Initial Build

--- main.js
+++ main.js
@@ -42,3 +42,14 @@ function isElementInViewport(el) {
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
