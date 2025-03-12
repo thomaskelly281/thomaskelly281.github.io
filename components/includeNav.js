d9872c96e6b95c19f55cf3b546874ddf50de1f10
Thomas Kelly <thomaskelly281@gmail.com>
2025-03-12 16:53:31 +0000

    Revert "V3 updates"

--- components/includeNav.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('/components/nav.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('nav-component').innerHTML = data;
-    });
-}); 
