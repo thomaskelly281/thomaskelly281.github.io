053ee22e9f036fe3243729f3592dc979e06ed851
Thomas Kelly <thomaskelly281@gmail.com>
2025-09-24 18:59:47 +0100

    Bug fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/thomaskelly281.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/thomaskelly281.github.io' : '',
 };
 
 export default nextConfig;
