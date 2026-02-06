9f70bd1c0af114e34256d8f6f53cefbab4a475dc
Thomas Kelly <thomaskelly281@gmail.com>
2026-02-06 12:35:51 +0100

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
       <body className="antialiased" suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem={false}
           disableTransitionOnChange={false}
         >
